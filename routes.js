64d24436290dcaf2a22a3c4a11bad8275516daf8
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2016-08-17 11:53:37 +0200

    fixed refresh after page change

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 route.on('download', function(url, params, unchanged){
     var reload=arguments.callee;
+    debugger;    
+    if(location.hash!='#'+url)
+        return;
     $.ajax(loadHtml('download-manager', function(){
-        setTimeout(function(){ reload(url, params, unchanged)}, 2000);
+        timeout=setTimeout(function(){ reload(url, params, unchanged)}, 2000);
     }));
 });
